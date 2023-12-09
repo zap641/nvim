7a45a89f207c7947a82457591fc180c9fd5c6b07
zap641 <121319707+zap641@users.noreply.github.com>
2023-12-10 08:42:59 +1000

    more updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,11 +21,10 @@ require('mason-lspconfig').setup({
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
   local opts = {buffer = bufnr, remap = false}
-
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader><CR>", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "<leader><backspace>", function() vim.diagnostic.goto_prev() end, opts)
 end)
 
 lsp.setup()
