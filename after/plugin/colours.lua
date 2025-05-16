83c7ad3d1472b875cc334979dadf82026f857a31
zap641 <121319707+zap641@users.noreply.github.com>
2025-05-17 06:43:25 +1000

    Delete after/plugin directory

--- after/plugin/colours.lua
+++
@@ -1,9 +0,0 @@
-function colour_my_nvim(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-colour_my_nvim()
